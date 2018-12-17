85919b7f9f72a5d59dda9d5d76ff442ac514860e
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-12-16 23:19:01 -0600

    Complete'

--- JS/index.js
+++ JS/index.js
@@ -2,4 +2,22 @@
 
 const arrayToTree = (array, indx = 0) => {
 
+	// If indx is out of range
+	if(indx > array.length) return;
+
+	// Indices for left and right children nodes
+	const left  = (indx * 2) + 1,
+		  right = left + 1;
+		   
+	// Using reccursion, returns object with values in tree form
+	return {
+		value: array[indx],
+
+		left: arrayToTree(array, left),
+
+		right: arrayToTree(array, right)
+	}	  
+
 }
+
+console.log(arrayToTree([10, 9, 8, 7, 6 ,5, 4, 3, 2, 1, 0]));
